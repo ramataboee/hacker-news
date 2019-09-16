9532bfd0ed9a78d143b1068a54ea5e8851ab0b6f
Mpho <mramataboee@nitssolutions.com>
2019-09-16 14:52:23 -0400

    added react-redux and used connect HOCs

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Stories.css';
 import Story from './Story';
 
+import {connect} from 'react-redux';
+import {doArchiveStory} from '../actions/archive';
+import {getReadableStories} from '../selectors/story';
+
 const Stories = ({stories, onArchive}) =>
   <div className='stories'>
     {(stories || []).map(story =>
@@ -13,4 +17,17 @@ const Stories = ({stories, onArchive}) =>
     )}
   </div>
 
-export default Stories;
+//subscribes to updates from redux store. on each store subscription update this will run
+const mapStateProps = state => ({
+  stories: getReadableStories(state),
+});
+
+//gives access to dispatch methods
+const mapDispatchToProps = dispatch => ({
+  onArchive: id => dispatch(doArchiveStory(id)),
+});
+
+export default connect(
+  mapStateProps,
+  mapDispatchToProps
+)(Stories);
