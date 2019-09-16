102e5dfe8414766a1829d4dba3d1c8825ecbd398
Mpho <mramataboee@nitssolutions.com>
2019-09-16 17:49:58 -0400

    added created date

--- src/components/Story.js
+++ src/components/Story.js
@@ -11,15 +11,17 @@ const Story = ({story, onArchive}) => {
     author,
     num_comments,
     points,
+    created_at,
     objectID,
   } = story;
 
   return (
     <div className='story'>
-      <span className='story-title'>
+      <span className='story-title semibold'>
         <a href={url}>{title}</a>
       </span>
       <span className='story-author'>{author}</span>
+      <span className='story-created-at'>{created_at}</span>
       <span className='story-comment'>{num_comments}</span>
       <span className='story-point'>{points}</span>
       <span>
